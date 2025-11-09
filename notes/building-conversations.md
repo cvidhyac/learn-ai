88b472cf5a1cd8b0bf8684d3d6ee9da7845840b2
Vidhya Chidambaram <vchidamb@redhat.com>
2025-11-09 18:21:12 -0500

    update notes for shot prompting, assistant messages

--- notes/building-conversations.md
+++ notes/building-conversations.md
@@ -51,3 +51,50 @@ respond with: "I am sorry, I am not allowed to provide financial advice.
 """
 ```
 
+## Shot Prompting
+
+- One shot prompting:
+  LLM uses the user message, any pre-existing system message and its trained knowledge
+  to answer the query. Typically single-turn tasks with static prompts are good examples of "one-shot"
+  prompting. The accuracy of the LLM response will need more iterations to achieve the desired output.
+
+- Few Shot prompting:
+  Utilizes the roles - system, user, and assistant to provide guardrails, precise instructions and examples.
+  Multi-turn conversations or more complex analysis usually are good use cases for few-shot prompting.
+
+### System vs assistant vs user - What is the best place to provide example?
+
+* System -> Important template formatting
+
+  For example:
+
+  ```
+  Output the information in this format
+  field_one | field_two | field_three
+  When the user asks questions unrelated to formatting, respond with 'I am not allowed to answer'
+  ```
+* User -> Context required for new input
+
+For example:
+
+  ```
+  Help me format this text to pipe delimited values: userA, userB, userC
+  ```
+
+* Assistant -> Example conversations
+
+For example:
+
+```
+/user: Help me format the line - fieldA, fieldB : fieldC to pipe-delimited value
+returns: fieldA | fieldB | fieldC
+
+/user: What is the weather today?
+returns: I am not allowed to answer
+```
+
+- Assistant messages are excellent places to provide examples in addition to the examples provided in system and user
+  roles. It is a more structured way in shot-prompting.
+- More examples provide the application more structure and more accurate responses.
+
+In a new system configuration, it is always advised to include as many "user-assistant" pairs as possible.
--- practice/learn-roles.py
+++ practice/learn-roles.py
@@ -8,24 +8,17 @@ from openai import OpenAI
 """
 Execute a prompt with given user and system role messages
 """
-def execute_prompt(user_prompt: str, system_prompt: str):
+def execute_prompt(input_msg: list[dict[str, str]]):
     client = OpenAI(base_url="https://openrouter.ai/api/v1",
                     api_key=os.getenv("OPENAI_API_KEY"))
     response = client.chat.completions.create(
         model="ibm-granite/granite-4.0-h-micro",
         max_completion_tokens=250,
         temperature=1,
-        messages=[{
-            "role": "system",
-            "content": system_prompt
-        },
-            {
-                "role": "user",
-                "content": user_prompt
-            }
-        ]
+        messages=input_msg
     )
     print(response.choices[0].message.content)
+    print("------- end prompt execution -------------")
 
 """
 Example using standard prompt without guardrails
@@ -33,7 +26,16 @@ Example using standard prompt without guardrails
 def ask_python_question():
     standard_sys_prompt = "You are a python tutor that speaks concisely"
     prompt = input("Enter a python question:")
-    execute_prompt(prompt, standard_sys_prompt)
+    msg = [{
+        "role": "system",
+        "content": standard_sys_prompt
+    },
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+    execute_prompt(msg)
 
 """
 Example applying a stronger guardrail in system message to prevent LLM misuse
@@ -43,9 +45,36 @@ def ask_question():
                coding or concepts, please answer 'Apologies, to focus on python, I am not allowed to answer other topics')
                """
     prompt = input("Enter another python question:")
-    execute_prompt(prompt, sys_prompt_with_guardrails)
 
+    msg = [{
+        "role": "system",
+        "content": sys_prompt_with_guardrails
+    },
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+
+    execute_prompt(msg)
+
+"""
+Example with system, user and assistant messages
+"""
+def applying_user_assistant_examples():
+    system_message = f"""You are a helpful geography tutor that generates concise summaries of 100 words for different 
+    countries. When asked about topics unreleated to geography respond with - 'I am not allowed to answer'"""
+    msg = [
+        { "role": "system", "content": system_message},
+        { "role": "user", "content": "What is the weather today?"},
+        { "role": "assistant", "content": "I am not allowed to answer"},
+        { "role": "user", "content": "Give me an overview of France"},
+        {"role": "assistant", "content": "France is a beautiful country. Paris is the capital of france"},
+        {"role": "user", "content": "Give me a summary of Spain's history"}
+    ]
+    execute_prompt(msg)
 
 if __name__ == '__main__':
     ask_python_question()
     ask_question()
+    applying_user_assistant_examples()
