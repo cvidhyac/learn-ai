dd7822a1e1966697c52e9bc4a74051aeb0fd6ecd
Vidhya Chidambaram <vchidamb@redhat.com>
2025-11-09 15:02:16 -0500

    update sentiment-analysis example

--- practice/learn-sentiment-classification.py
+++ practice/learn-sentiment-classification.py
@@ -8,23 +8,57 @@ There are two ways an LLM learns:
 
 When given examples and/or step by step prompting there are many math yardsticks that prove
 that the output received from the LLM becomes more deterministic.
+
+As part of this learning, i wrote two examples, one that uses few-shot prompting, then another
+with fixed formatting.
 """
 
-# define a multi-line prompt to classify sentiment
 
-prompt = """ Classify the customer sentiment for the reviews from an online store"
-1. Unbelievably good! -> 5
-2. Shoes fell apart on the second use. -> 1
-3. The shoes look nice, but they aren't very comfortable.
-4. Can't wait to show them off!
 """
+Example 1: 
+Define a multi-line prompt to give a couple of examples against each prompt
+We specify arrows and aren't very particular on formatting.
+"""
+def basic_few_shot_prompting(client:OpenAI) -> None:
+    prompt = """ Classify the customer sentiment for the reviews from an online store"
+    1. Unbelievably good! -> 5
+    2. Shoes fell apart on the second use. -> 1
+    3. The shoes look nice, but they aren't very comfortable.
+    4. Can't wait to show them off!
+    """
+    execute_prompt(client, prompt)
 
-client = OpenAI(base_url="https://openrouter.ai/api/v1",
-                api_key=os.getenv("OPENAI_API_KEY"))
+"""
+Here we add examples similar to previous one, however give LLM an indication to reduce verbosity
+and stick format by indicating we only need `=` and the result.
 
-# create a request to chat completions endpoint
+The result is that LLM picks up the cue and provides the response what we need with precise instructions
+"""
+def improved_few_shot_prompting(client:OpenAI) -> None:
+    prompt = """ Classify sentiment as 1-5 (negative to positive)"
+    1. Unbelievably good! = 5
+    2. Shoes fell apart on the second use. = 1
+    3. The shoes look nice, but they aren't very comfortable. =
+    4. Can't wait to show them off! =
+    5. The quality was not what i expected = 
+    """
+    execute_prompt(client, prompt)
 
-response = client.chat.completions.create(model="minimax/minimax-m2:free",
-                                          messages=[{"role": "user", "content": prompt}],
-                                          max_completion_tokens=400, temperature=1)
-print(response.choices[0].message.content)
+"""
+Utility method that defines a pre-defined model for this example
+We limit to 250 tokens here and keep a static temperature of 1, since intent is to understand how
+sentiment classification is interpreted by LLM
+"""
+def execute_prompt(client, prompt):
+    response = client.chat.completions.create(model="gpt-4o-mini",
+                                              messages=[{"role": "user", "content": prompt}],
+                                              max_completion_tokens=300, temperature=1)
+    print(response.choices[0].message.content)
+    print("---------------end prompt execution ------------------")
+
+
+if __name__ == '__main__':
+    openai_client = OpenAI(base_url="https://openrouter.ai/api/v1",
+                    api_key=os.getenv("OPENAI_API_KEY"))
+    basic_few_shot_prompting(openai_client)
+    improved_few_shot_prompting(openai_client)
